74409993c654551e62b7938fb6ce6326d6b0c78c
Stephane Moyart <moyartstephane@gmail.com>
2024-12-23 08:43:53 +0100

    create footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import { Footer } from "./components/footer";
 import { HeroSection } from "./components/hero";
 
 export default function Home() {
-    return ( 
-        <HeroSection/>
+    return (
+        <>
+            <HeroSection/>
+            
+            
+            <Footer/>
+        </> 
     )
 }
