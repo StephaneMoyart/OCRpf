a5d283f1d858721e71de7a7dbfe6376d966c4c41
Stephane Moyart <moyartstephane@gmail.com>
2024-12-30 18:45:05 +0100

    imrove projects section

--- src/app/components/project-card.tsx
+++ src/app/components/project-card.tsx
@@ -2,10 +2,11 @@
 
 import { motion, useScroll, useTransform } from "motion/react"
 import Image from "next/image"
-import { useRef } from "react"
+import Link from "next/link"
+import { useRef, useState } from "react"
 
 export const ProjectCard = ({ card, containerProgress }) => {
-    const {id, title, mainPicture, secondaryPicture, techs, color, translateY, range, targetScale} = card
+    const {id, title, mainPicture, secondaryPicture, techs, description, tasks, color, githubLink, translateY, range, targetScale} = card
     const cardRef = useRef(null)
     const { scrollYProgress } = useScroll({
         target: cardRef,
@@ -15,15 +16,17 @@ export const ProjectCard = ({ card, containerProgress }) => {
     const borderTopLeftRadius = useTransform(scrollYProgress, [0, 1], ["0", "20px"])
     const borderTopRightRadius = useTransform(scrollYProgress, [0, 1], ["0", "20px"])
     const borderRadius = useTransform(scrollYProgress, [0, 1], ["0", "20px"])
-
+    
     const scale = useTransform(containerProgress, [range, 1], [1, targetScale])
+
+    const [isClicked, setIsClicked] = useState(false)
 
     if (id === 3) {
         return (
             <div ref={cardRef} className="h-dvh w-full flex items-end justify-center sticky top-0">
                 <motion.div 
                     style={{ scaleX, borderRadius, scale, transformOrigin: "top" }}
-                    className={`h-[90%] w-full flex items-center justify-center ${color} relative ${translateY}`}
+                    className={`h-[95%] w-full flex items-center justify-center ${color} relative ${translateY}`}
                 >
                     {id === 3 &&
                         <p className="text-foreground text-3xl">D'autres projets sont en cours de création...</p>
@@ -35,29 +38,71 @@ export const ProjectCard = ({ card, containerProgress }) => {
 
     return (
         <div ref={cardRef} className="h-dvh w-full flex items-end justify-center sticky top-0">
-            <motion.div 
+            <motion.div
+                onClick={() => setIsClicked(prev => !prev)}
                 style={{ scaleX, borderTopLeftRadius, borderTopRightRadius, scale, transformOrigin: "top" }}
-                className={`h-[90%] w-full flex flex-col justify-center px-40 ${color} relative ${translateY}`}
+                className={`h-[95%] w-full relative ${translateY} group [perspective:5000px]`}
             >
-                <div className="flex shrink-0 items-center justify-between w-full h-60">
-                    <h3 className="text-6xl font-bold">{title}</h3>
-                    <div className="flex gap-4">
-                        {techs.map((tech, index) => (
-                            <div key={index} className="py-2 px-4 rounded-3xl border-dashed border border-white">
-                                <p>{tech}</p>
+                <motion.div style={{ borderTopLeftRadius, borderTopRightRadius }} className={`h-full w-full ${color} transition-[transform] duration-1000 ease-in-out [transform-style:preserve-3d] ${isClicked ? "[transform:rotateY(180deg)]" : ""}`}>
+                    <div className="h-full w-full flex flex-col items-center px-40 [backface-visibility:hidden]">
+                        <div className="flex shrink-0 items-center justify-between w-full h-60">
+                            <h3 className="text-6xl">{title}</h3>
+                            <div className="flex gap-4">
+                                {techs?.map((tech, index) => (
+                                    <div key={index} className="py-2 px-4 rounded-3xl border-dashed border border-white">
+                                        <p>{tech}</p>
+                                    </div>
+                                ))}
                             </div>
-                        ))}
+                        </div>
+                        <div className="relative h-full w-full">
+                            <Image
+                                className="rounded-t-[20px]"
+                                src={mainPicture}
+                                alt="image principale du projet"
+                                fill
+                                
+                            />
+                        </div>
                     </div>
-                </div>
-                <div className="relative h-full w-full">
-                    <Image
-                        className="rounded-t-[20px]"
-                        src={mainPicture}
-                        alt="image principale du projet"
-                        fill
+                    <motion.div style={{ borderTopLeftRadius, borderTopRightRadius }} className="absolute inset-0 h-full w-full grid grid-cols-2 grid-rows-2 [transform:rotateY(180deg)] [backface-visibility:hidden]">
+                        <div style={{ borderTopLeftRadius }} className="relative">
+                            <motion.img
+                                style={{ borderTopLeftRadius }}
+                                src={mainPicture}
+                                alt="image principale du projet"
+                                className="w-full h-full"
+                            />
+                        </div>
+                        <div className="flex flex-col items-center justify-evenly">
+                            <h3 className="text-3xl">{title}</h3>
+                            <div className="flex gap-4">
+                                {techs?.map((tech, index) => (
+                                    <div key={index} className="py-2 px-4 rounded-3xl border-dashed border border-white">
+                                        <p>{tech}</p>
+                                    </div>
+                                ))}
+                            </div>
+                            <p>{description}</p>
+                            <a href={githubLink} target="_blank">
+                                <svg 
+                                    onClick={e => e.stopPropagation()}
+                                    className="cursor-pointer max-md:w-40" width="70" viewBox="0 0 256 249" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet"><g fill="rgb(248, 248, 248)"><path d="M127.505 0C57.095 0 0 57.085 0 127.505c0 56.336 36.534 104.13 87.196 120.99 6.372 1.18 8.712-2.766 8.712-6.134 0-3.04-.119-13.085-.173-23.739-35.473 7.713-42.958-15.044-42.958-15.044-5.8-14.738-14.157-18.656-14.157-18.656-11.568-7.914.872-7.752.872-7.752 12.804.9 19.546 13.14 19.546 13.14 11.372 19.493 29.828 13.857 37.104 10.6 1.144-8.242 4.449-13.866 8.095-17.05-28.32-3.225-58.092-14.158-58.092-63.014 0-13.92 4.981-25.295 13.138-34.224-1.324-3.212-5.688-16.18 1.235-33.743 0 0 10.707-3.427 35.073 13.07 10.17-2.826 21.078-4.242 31.914-4.29 10.836.048 21.752 1.464 31.942 4.29 24.337-16.497 35.029-13.07 35.029-13.07 6.94 17.563 2.574 30.531 1.25 33.743 8.175 8.929 13.122 20.303 13.122 34.224 0 48.972-29.828 59.756-58.22 62.912 4.573 3.957 8.648 11.717 8.648 23.612 0 17.06-.148 30.791-.148 34.991 0 3.393 2.295 7.369 8.759 6.117 50.634-16.879 87.122-64.656 87.122-120.973C255.009 57.085 197.922 0 127.505 0"/><path d="M47.755 181.634c-.28.633-1.278.823-2.185.389-.925-.416-1.445-1.28-1.145-1.916.275-.652 1.273-.834 2.196-.396.927.415 1.455 1.287 1.134 1.923M54.027 187.23c-.608.564-1.797.302-2.604-.589-.834-.889-.99-2.077-.373-2.65.627-.563 1.78-.3 2.616.59.834.899.996 2.08.36 2.65M58.33 194.39c-.782.543-2.06.034-2.849-1.1-.781-1.133-.781-2.493.017-3.038.792-.545 2.05-.055 2.85 1.07.78 1.153.78 2.513-.019 3.069M65.606 202.683c-.699.77-2.187.564-3.277-.488-1.114-1.028-1.425-2.487-.724-3.258.707-.772 2.204-.555 3.302.488 1.107 1.026 1.445 2.496.7 3.258M75.01 205.483c-.307.998-1.741 1.452-3.185 1.028-1.442-.437-2.386-1.607-2.095-2.616.3-1.005 1.74-1.478 3.195-1.024 1.44.435 2.386 1.596 2.086 2.612M85.714 206.67c.036 1.052-1.189 1.924-2.705 1.943-1.525.033-2.758-.818-2.774-1.852 0-1.062 1.197-1.926 2.721-1.951 1.516-.03 2.758.815 2.758 1.86M96.228 206.267c.182 1.026-.872 2.08-2.377 2.36-1.48.27-2.85-.363-3.039-1.38-.184-1.052.89-2.105 2.367-2.378 1.508-.262 2.857.355 3.049 1.398"/></g>
+                                </svg>
+                            </a>
+                        </div>
+                        <div className="flex flex-col items-center justify-evenly">
+                            {tasks?.map((task, index) => (
+                                <p key={index}>{task}</p>
+                            ))}
+                        </div>
                         
-                    />
-                </div>
+                        <img
+                            src={secondaryPicture}
+                            alt="image secondaire du projet"
+                        />
+                    </motion.div>
+                </motion.div>
             </motion.div>
         </div>
     )
--- src/app/components/projects.tsx
+++ src/app/components/projects.tsx
@@ -12,7 +12,10 @@ export const Projects = () => {
             mainPicture: "/kasa1.png",
             secondaryPicture: "/kasa2.png",
             techs: ["React", "Sass"],
+            description: "Création du front-end d'une application web de location immobilière en utilisant React et Sass.",
+            tasks: ["Création de 0 avec React/React router", "Reflexion en terme de composants réutilisables", "Design responsive desktop / tablette / mobile à partir d'une maquette Figma"],
             color: "bg-zinc-800",
+            githubLink: "https://github.com/StephaneMoyart/Projet5",
             translateY: "top-[10px]",
             range: 1/3,
             targetScale: 0.95
@@ -23,7 +26,10 @@ export const Projects = () => {
             mainPicture: "/mvg1.png",
             secondaryPicture: "/mvg2.png",
             techs: ["NodeJs", "Express", "Mongoose", "MongoDB"],
+            description: "Developpement du back-end d'un site communautaire de présentation et notation de livres.",
+            tasks: ["Création d'un serveur Express", "Connexion à une base de données mongoDB", "Modélisation de données via Mongoose", "Création d'API RESTful"],
             color: "bg-zinc-700",
+            githubLink: "https://github.com/StephaneMoyart/Projet6",
             translateY: "top-[20px]",
             range: 2/3,
             targetScale: 0.975
