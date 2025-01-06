86a7a7e674ccc1e02fac3a75e1001d7c6d7f007a
Stephane Moyart <moyartstephane@gmail.com>
2025-01-06 03:00:36 +0100

    fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,19 +6,23 @@ import { HeroSection } from "./components/hero";
 import { Projects } from "./components/projects";
 import { Skills } from "./components/skills";
 import { Stack } from "./components/stack";
+import { useEffect } from "react";
 
 export default function Home() {
-    const lenis = new Lenis({
-        lerp: 0.05, 
-        wheelMultiplier: 1, 
-    })
-
-    function raf(time: any) {
-        lenis.raf(time)
+    
+    useEffect(() => {
+        const lenis = new Lenis({
+            lerp: 0.05, 
+            wheelMultiplier: 1, 
+        })
+    
+        function raf(time: any) {
+            lenis.raf(time)
+            requestAnimationFrame(raf)
+        }
+    
         requestAnimationFrame(raf)
-    }
-
-    requestAnimationFrame(raf)
+    }, [])
 
     return (
         <>
